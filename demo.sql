1647ef4a71dd873378feb77046ed5415cb60d7ae
Benjamin Block <bjblock@gmail.com>
2024-01-11 01:37:07 +0000

    completed lab 1 sql queries

--- demo.sql
+++ demo.sql
@@ -1,3 +1,10 @@
 -- Get the first and last name of every player in the database.
 
 SELECT first_name, last_name FROM players;
+
+-- sqlite3 baseball.sqlite3
+-- .schema
+-- .mode columns
+-- .headers on
+-- .read ______
+-- .quit
